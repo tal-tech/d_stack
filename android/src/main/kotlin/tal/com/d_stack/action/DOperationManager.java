3648f5fff682d0a835cc2b7c031f4f2e7be6070a
maxu <maxu@tal.com>
2020-12-10 19:10:23 +0800

    修改bug

--- android/src/main/kotlin/tal/com/d_stack/action/DOperationManager.java
+++ android/src/main/kotlin/tal/com/d_stack/action/DOperationManager.java
@@ -18,11 +18,26 @@ public class DOperationManager {
         if (!DStack.getInstance().isOpenNodeOperation()) {
             return;
         }
-        if (!node.isBoundary() &&
-                node.getPageType().equals(DNodePageType.DNodePageTypeFlutter)
-                && node.getAction().equals(DNodeActionType.DNodeActionTypePop)) {
-            //操作一个flutter页面如果是pop行为，不是临界页面，不记录操作，等didPop消息
-            return;
+        if (node.getAction().equals(DNodeActionType.DNodeActionTypePush)) {
+            //页面如果是push行为
+            if (node.isFromFlutter()) {
+                //如果是来自flutter的消息
+                if (node.getPageType().equals(DNodePageType.DNodePageTypeNative)) {
+                    //如果是打开一个native页面，不记录操作，等activity的onCreate方法
+                    return;
+                }
+
+            }
+        }
+        if (node.getAction().equals(DNodeActionType.DNodeActionTypePop)) {
+            //页面如果是pop行为
+            if (!node.isBoundary()) {
+                //不是临界页面
+                if (node.getPageType().equals(DNodePageType.DNodePageTypeFlutter)) {
+                    //操作一个flutter页面，不记录操作，等didPop消息
+                    return;
+                }
+            }
         }
         DNodeResponse nodeResponse = DNodeManager.getInstance().createNodeResponse(node);
         DLog.logE("$$$$$节点操作$$$$$");
